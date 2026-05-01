aac142615a2a92213d8f2347eaba3161a5c2e40a
camjayreed <xtswolf2003@gmail.com>
2026-05-01 07:33:27 -0500

    implemented a way to track current user, and properly formatted users messages

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 document.getElementById("join-chat").addEventListener("click", connectUserToChat)
 function connectUserToChat() {
     console.log('user connected')
-    /* set current user to x in localstorage */
+    localStorage.setItem("CurrentUser", document.getElementById("username").value)
     /* initiate websocket connection */
     /* grab and display all previous messages */ 
     return
@@ -19,6 +19,7 @@ function userLeaveChat() {
 /* if user sends message */
 document.getElementById("send-message").addEventListener("click", sendUserMessage)
 function sendUserMessage() {
-    console.log('user sent message')
+    const userMessage = document.getElementById("user-message-input").value
+    const usernamePlusMessage = localStorage.getItem("CurrentUser").concat(": ", userMessage)
     return
 }
