4593b9f4acf758078430bd4e8c5beabd3621d87b
camjayreed <xtswolf2003@gmail.com>
2026-05-02 07:49:14 -0500

    actually broadcasting messages to all users now

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const WebSocket = require('ws');
-const ws = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080 });
 
 console.log('WebSocket server is running on port 8080');
 
-ws.on('connection', (ws) => {
+wss.on('connection', (ws) => {
   console.log('New client connected');
 
   ws.on('error', console.error);
 
   /* if we get message */
   ws.on('message', (data) => {
-    console.log(`User message received: "${data.toString()}"`)
-    ws.send(data.toString());
-    console.log("Message sent to all users!")
+    wss.clients.forEach(function(client) {
+        console.log(`User message received: "${data.toString()}"`)
+        client.send(data.toString());
+        console.log("Message sent to all users!")
+    });
   });
 
   ws.on('close', () => {
