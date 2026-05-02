ec0765a6db088e562c41b368bbd47f63b5a48557
camjayreed <xtswolf2003@gmail.com>
2026-05-02 06:52:06 -0500

    implemented logic for websockets and realtime chat, currently receiving object blobs and cant figure out how to convert them to strings for display.

--- scripts.js
+++ scripts.js
@@ -1,26 +1,45 @@
+var ws = null;
+
 /* if user joins chat */
-document.getElementById("join-chat").addEventListener("click", connectUserToChat)
+document.getElementById("join-chat").addEventListener("click", connectUserToChat);
 function connectUserToChat() {
-    console.log('user connected')
-    document.getElementById("user-chatroom").removeAttribute("hidden")
-    localStorage.setItem("CurrentUser", document.getElementById("username").value)
-    /* initiate websocket connection */
-    /* grab and display all previous messages */ 
-    return
+  document.getElementById("join-chat").setAttribute("hidden", true);
+  console.log("user connected");
+  document.getElementById("user-chatroom").removeAttribute("hidden");
+  localStorage.setItem(
+    "CurrentUser",
+    document.getElementById("username").value,
+  );
+
+  ws = new WebSocket("ws://127.0.0.1:8080");
+  ws.addEventListener("message", (event) => {
+    const chatroom = document.getElementById("user-chatroom");
+    const newMessage = document.createTextNode(event.data);
+
+    chatroom.appendChild(newMessage);
+    })
+  /* grab and display all previous messages */
+  return;
 }
 
 /* if user leaves chatroom */
-document.getElementById("leave-chat").addEventListener("click", userLeaveChat)
+document.getElementById("leave-chat").addEventListener("click", userLeaveChat);
 function userLeaveChat() {
-    console.log('user disconnected')
-    document.getElementById("user-chatroom").setAttribute("hidden", true)
-    return
+  ws.close();
+  console.log("user disconnected");
+  document.getElementById("user-chatroom").setAttribute("hidden", true);
+  document.getElementById("join-chat").removeAttribute("hidden");
+  return;
 }
 
 /* if user sends message */
-document.getElementById("send-message").addEventListener("click", sendUserMessage)
+document.getElementById("send-message").addEventListener("click", sendUserMessage);
 function sendUserMessage() {
-    const userMessage = document.getElementById("user-message-input").value
-    const usernamePlusMessage = localStorage.getItem("CurrentUser").concat(": ", userMessage)
-    return
+  const userMessage = document.getElementById("user-message-input").value;
+  const usernamePlusMessage = localStorage.getItem("CurrentUser").concat(": ", userMessage);
+
+  /* send message to websocket */
+  ws.send(usernamePlusMessage)
+  console.log(usernamePlusMessage);
+  return;
 }
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+const ws = new WebSocket.Server({ port: 8080 });
 
 console.log('WebSocket server is running on port 8080');
 
-wss.on('connection', (ws) => {
+ws.on('connection', (ws) => {
   console.log('New client connected');
+
+  ws.on('error', console.error);
+
+  /* if we get message */
+  ws.on('message', (data) => {
+    console.log(data.toString())
+
+    ws.send(data.toString());
+  });
 
   ws.on('close', () => {
     console.log('Client disconnected');
